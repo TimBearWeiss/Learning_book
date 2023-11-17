5a352931f9236d595f2aaebeaff2f4fa9d234453
Dmitrii Zverev <zverevtimbear@yandex.ru>
2023-11-17 23:56:58 +0300

    второй коммит

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -8,6 +8,13 @@ function App() {
   const Boolean = true;
   const object = { firstName: "Dmitrii", lastName: "Ivanov", userArr: arr };
 
+  /// work with string
+
+  // первый найденный будем записан в переменную
+  const currentCount = arr.find((el) => el > 2);
+
+  console.log(currentCount);
+
   console.log(object);
 
   return <div className="App">{string}</div>;
